d4a0ec411224e28f8b21b8b705be89f885595155
hanep <yudishan26@gmail.com>
2024-02-11 22:36:11 +0700

    create about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import GoDown from './components/GoDown';
+import { PiStudent, PiStudentBold } from "react-icons/pi";
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
@@ -10,21 +11,52 @@ function App() {
   }
 
   return (
-    <div className={`${darkMode ? 'bg-slate-900 text-slate-100' : 'bg-slate-200 text-slate-800'} transition`}>
+    <div className={`${darkMode ? 'bg-slate-900 text-slate-400' : 'bg-slate-200 text-slate-600'} transition`}>
       <Navbar darkMode={darkMode} onToggleDarkMode={handleToggleDarkMode} />
       <main>
         <section id='home'>
           <div className='max-w-screen-md min-h-screen mx-auto flex flex-col-reverse md:flex-row items-center justify-center px-4 md:px-0 gap-4'>
             <div className='md:w-2/3 flex flex-col items-center md:items-start'>
-              <p className='text:lg md:text-xl text-slate-500'>Hi, my name is...</p>
-              <h1 className='text-5xl md:text-7xl font-bold mt-1'>Yudis Sutisna</h1>
+              <p className='text:lg md:text-xl'>Hi, my name is...</p>
+              <h1 className={`text-5xl md:text-7xl font-bold mt-1 ${darkMode ? 'text-slate-100' : 'text-slate-800'}`}>Yudis Sutisna</h1>
               <p className={`${darkMode ? 'text-orange-500' : 'text-orange-700'} mt-2 text-lg md:text-2xl font-medium`}>Welcome to my portfolio website</p>
-              <GoDown />
+              <GoDown darkMode={darkMode} />
             </div>
             <div className='flex md:w-1/3 justify-end items-center'>
               <div className='w-48 md:w-60 rounded-full overflow-hidden border-4 border-orange-700'>
                 <img src="/image/me.jpg" alt='gw' />
               </div>
+            </div>
+          </div>
+        </section>
+
+        <section id='about'>
+          <div className='max-w-screen-md min-h-screen mx-auto px-4 py-16 md:px-0'>
+            <h2 className={`text-3xl font-bold text-center ${darkMode ? 'text-slate-100' : 'text-slate-800'}`}>About Me</h2>
+            <p className='pt-5 text-justify md:text-center text-lg'>Nama saya Yudis Sutisna biasa di panggil Hanep. Saya seorang siswa yang sedang bersekolah di SMK Al Huda Sariwangi dan mengambil jurusan Rekayasa Perangkat Lunak. Saat ini saya sedang belajar hal-hal seputar ilmu komputer khususnya di bidang Web Programming.</p>
+            <div className='mt-12 flex gap-8 flex-col md:flex-row'>
+              <div className='md:w-1/2'>
+                <h2 className={`text-3xl font-bold ${darkMode ? 'text-slate-100' : 'text-slate-800'}`}>Education</h2>
+                <div className='border-l-2 border-orange-700 px-6 relative mt-5 pb-5 mx-2'>
+                  <div className='w-6 h-6 bg-orange-700 text-slate-100 absolute -top-0 -left-3 rounded-full flex items-center justify-center'><PiStudentBold /></div>
+                  <h4 className={`${darkMode ? 'text-slate-100' : 'text-slate-800'} text-xl font-semibold`}>SDN 2 Salawu</h4>
+                  <p className='text-sm'>2012 - 2018</p>
+                </div>
+
+                <div className='border-l-2 border-orange-700 px-6 relative pb-5 mx-2'>
+                  <div className='w-6 h-6 bg-orange-700 text-slate-100 absolute -top-0 -left-3 rounded-full flex items-center justify-center'><PiStudentBold /></div>
+                  <h4 className={`${darkMode ? 'text-slate-100' : 'text-slate-800'} text-xl font-semibold`}>SMPN 2 Sariwangi</h4>
+                  <p className='text-sm'>2018 - 2021</p>
+                </div>
+
+                <div className='border-l-2 border-orange-700 px-6 relative mx-2'>
+                  <div className='w-6 h-6 bg-orange-700 text-slate-100 absolute -top-0 -left-3 rounded-full flex items-center justify-center '><PiStudentBold /></div>
+                  <h4 className={`${darkMode ? 'text-slate-100' : 'text-slate-800'} text-xl font-semibold`}>SMK AL-HUDA Sariwangi</h4>
+                  <p>Rekayasa Perangkat Lunak</p>
+                  <p className='text-sm'>2012 - 2018</p>
+                </div>
+              </div>
+              <div className='text-9xl flex justify-center items-center md:w-1/2 text-orange-700 py-8 border-4 rounded-md border-orange-700'><PiStudent /></div>
             </div>
           </div>
         </section>
--- src/components/GoDown.jsx
+++ src/components/GoDown.jsx
@@ -1,13 +1,13 @@
 import { FaArrowDown } from "react-icons/fa";
 
-export default function () {
+export default function ({ darkMode }) {
     return (
         <div className="flex flex-col items-start gap-1 translate-y-8 text-slate-200">
             <div className='flex flex-col items-center'>
                 <a href="#about" className="w-16 h-16 flex justify-center items-center bg-orange-700 rounded-full animate-bounce text-2xl">
                     <FaArrowDown />
                 </a>
-                <p className="tracking-wider text-xs text-slate-600">Discover More</p>
+                <p className={`tracking-wider text-xs ${darkMode ? 'text-slate-600' : 'text-slate-400'}`}>Discover More</p>
             </div>
         </div>
     )
